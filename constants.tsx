c0a084d0048f80dce2b42726c8f618a066a7b197
THEJAS SREENIVASU <124007430+Thejas10042001@users.noreply.github.com>
2026-03-04 22:58:22 +0530

    refactor: Remove unused ChevronDown icon
    The ChevronDown icon component was present in constants.tsx but was not being utilized anywhere in the application. This commit removes it to declutter the constants file.

--- constants.tsx
+++ constants.tsx
@@ -172,9 +172,4 @@ export const ICONS = {
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
     </svg>
   ),
-  ChevronDown: ({ className }: { className?: string } = {}) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className || "w-5 h-5"} fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
-    </svg>
-  ),
 };
