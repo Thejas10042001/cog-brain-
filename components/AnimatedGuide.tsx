e93bd1ca01bd659970d3a5c68dc74affea90ed99
THEJAS SREENIVASU <124007430+Thejas10042001@users.noreply.github.com>
2026-03-13 16:01:02 +0530

    feat: Add Microphone icon to constants
    Introduces a new SVG icon for a microphone to the application's constant icon definitions. This will be used for future UI elements related to audio input.

--- components/AnimatedGuide.tsx
+++ components/AnimatedGuide.tsx
@@ -297,4 +297,3 @@ export const AnimatedGuide: React.FC<AnimatedGuideProps> = ({ type }) => {
       return null;
   }
 };
-
--- components/HelpCenter.tsx
+++ components/HelpCenter.tsx
@@ -289,4 +289,3 @@ export const HelpCenter: React.FC = () => {
   );
 };
 
-
